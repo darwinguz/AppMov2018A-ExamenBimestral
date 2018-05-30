9a9797d1ff4adae9001e973ad91111e4d9404b6a
Darwin Guzmán <dga_guz@hotmail.es>
2018-05-30 12:35:13 -0500

    Formulario de Crear Comida listo

--- app/src/main/java/cynitech/wrad/examenbimestral/CrearComidaActivity.kt
+++ app/src/main/java/cynitech/wrad/examenbimestral/CrearComidaActivity.kt
@@ -1,12 +1,36 @@
 package cynitech.wrad.examenbimestral
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_crear_comida.*
 
 class CrearComidaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crear_comida)
+
+        btn_guardar_comida.setOnClickListener(View.OnClickListener {
+            irListarComida()
+        })
+
+        btn_cancelar_comida.setOnClickListener(View.OnClickListener {
+            irMenuPrincipal()
+        })
     }
+
+
+    private fun irListarComida() {
+        val intent = Intent(this, ListarComidaActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun irMenuPrincipal() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+
 }
